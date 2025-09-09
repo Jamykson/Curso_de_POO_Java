ee8fd994d76b7715a6237baf56db5fa7cfa31455
Jamykson Freitas <105256874+Jamykson@users.noreply.github.com>
2025-09-09 20:52:53 +0000

    ComecoAula09

---
+++ aula09/Aula09.java
@@ -0,0 +1,11 @@
+package aula09;
+
+public class Aula09{
+
+    public static void main(String[] args) {
+
+        Pessoa alguem = new Pessoa("Cadu", "Masculino", 26);
+        alguem.apresentarPessoa();
+
+    }
+}
---
+++ aula09/Pessoa.java
@@ -0,0 +1,44 @@
+package aula09;
+public class Pessoa {
+
+    public String nome;
+    public int idade;
+    public String sexo;
+
+    public Pessoa( String nome, String sexo, int idade){ //Construtor
+        
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+
+    }
+
+    private void fazerAniver(){ 
+        System.out.println(getNome() + " está fazendo aniversário!");
+        setIdade(getIdade() + 1);
+    }
+
+    public void apresentarPessoa(){
+        System.out.println(getNome() + " tem " + getIdade() + " anos e é do sexo " + getSexo() + ".");
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public String getSexo() {
+        return sexo;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
